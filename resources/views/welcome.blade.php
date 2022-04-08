eb6e5949dc4b83bb745bf54ee0f25751f12af891
ATHIRA VIJAYAN <34649227+AthiraViju@users.noreply.github.com>
2022-04-08 11:15:45 +0530

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="facebook-domain-verification" content="g1xmglz1t51mly6hjtcktosp20lfi4" />
 
         <title>Laravel</title>
 
